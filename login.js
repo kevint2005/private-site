336a80dbe7e09d7cd192c1e47994f4e4323535ee
kevint2005 <83583613+kevint2005@users.noreply.github.com>
2021-05-05 09:12:10 +0200

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,8 @@ function validate(){
 var username=document.getElementById("username").value;
 var password=document.getElementById("password").value
 if(username=="kevin"&& password=="Kkevinvip8" 
-    || username=="ila"&& password=="mamtapecr" ){
+    || username=="ila"&& password=="mamtapecr" 
+   || username=="daniele" && password=="daniele05" ){
     window.location.href = 'index 2.html'
 } else{
     alert("login failed"); 

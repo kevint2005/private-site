a0e1b3fcbcc07443384d25909a4086c630c0e5b3
kevint2005 <83583613+kevint2005@users.noreply.github.com>
2021-05-05 10:27:53 +0200

    Add files via upload

--- login.js
+++ login.js
@@ -1,7 +1,11 @@
 function validate(){
 var username=document.getElementById("username").value;
 var password=document.getElementById("password").value
-if( username=="ila"&& password=="mamtapecr" ){
+if( 
+username=="ila"&& password=="mamtapecr" ||
+username=="daniele"&& password=="Mamt69" ||
+username=="mattia"&& password=="matti05" ||
+username=="michele"&& password=="mikk05" ){
     window.location.href = 'index 2.html'
 } else{ 
     if(username=="kevin"&& password=="Kkevinvip8"){

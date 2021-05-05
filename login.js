9070b0ce19e86d9a6e8b58fa4eb204160ce427ec
kevint2005 <83583613+kevint2005@users.noreply.github.com>
2021-05-05 17:04:20 +0200

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,8 @@ if(
 username=="ila"&& password=="mamtapecr" ||
 username=="daniele"&& password=="Mamt69" ||
 username=="mattia"&& password=="matti05" ||
-username=="michele"&& password=="mikk05" ){
+username=="michele"&& password=="mikk05" ||
+username=="christian"&& password=="chri05"){
     window.location.href = 'index 2.html'
 } else{ 
     if(username=="kevin"&& password=="Kkevinvip8"){
